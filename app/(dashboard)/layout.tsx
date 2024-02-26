834adeb49c5d0375f1f2ba1c56fab1fe093af4ef
Beterraba <ls6182315@gmail.com>
2024-02-26 09:26:25 -0300

    dashboard layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+
+import Sidebar from "@/components/Sidebar";
+import OrgSidebar from "@/components/OrgSidebar";
+import Navbar from "@/components/Navbar";
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayout;
