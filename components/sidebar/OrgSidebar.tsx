48640663c8c197fd0e2eb8f51be51a2f85e0fe6a
Beterraba <ls6182315@gmail.com>
2024-02-26 18:22:26 -0300

    navbar

--- components/sidebar/OrgSidebar.tsx
+++ components/sidebar/OrgSidebar.tsx
@@ -22,7 +22,7 @@ const OrgSidebar = () => {
     <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
       <Link href="/">
         <div className="flex items-center gap-x-2">
-          <Image src="/logo.svg" alt="logo" height={60} width={60} />
+          <Image src="/logo.svg" alt="Logo" height={60} width={60} />
           <span className={cn("font-semibold text-2xl", font.className)}>
             Board
           </span>
@@ -42,34 +42,42 @@ const OrgSidebar = () => {
               padding: "6px",
               width: "100%",
               borderRadius: "8px",
+              border: "1px solid #E5E7EB",
               justifyContent: "space-between",
               backgroundColor: "white",
             },
           },
         }}
       />
-      <Button
-        asChild
-        size="lg"
-        className="font-normal justify-start px-2 w-full"
-        variant={favorites ? "ghost" : "secondary"}
-      >
-        <Link href="/">
-          <LayoutDashboard className="h-4 w-4 mr-2" />
-          Team boards
-        </Link>
-      </Button>
-      <Button
-        asChild
-        size="lg"
-        className="font-normal justify-start px-2 w-full"
-        variant={!favorites ? "ghost" : "secondary"}
-      >
-        <Link href={{ pathname: "/", query: { favorites: true } }}>
-          <Star className="h-4 w-4 mr-2" />
-          Favorites boards
-        </Link>
-      </Button>
+      <div className="space-y-1 w-full">
+        <Button
+          variant={favorites ? "ghost" : "secondary"}
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+            Team boards
+          </Link>
+        </Button>
+        <Button
+          variant={favorites ? "secondary" : "ghost"}
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link
+            href={{
+              pathname: "/",
+              query: { favorites: true },
+            }}
+          >
+            <Star className="h-4 w-4 mr-2" />
+            Favorite boards
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
