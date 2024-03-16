bdb1cc97c1bc62c64b0c164871c4d2d63a5709ac
Beterraba <ls6182315@gmail.com>
2024-03-16 18:52:07 -0300

    canvas states

--- components/canvas/Canvas.tsx
+++ components/canvas/Canvas.tsx
@@ -1,5 +1,30 @@
 "use client";
 
+import { nanoid } from "nanoid";
+import { useCallback, useMemo, useState, useEffect } from "react";
+import { LiveObject } from "@liveblocks/client";
+
+import {
+  useHistory,
+  useCanUndo,
+  useCanRedo,
+  useMutation,
+  useStorage,
+  useOthersMapped,
+  useSelf,
+} from "@/liveblocks.config";
+
+import {
+  Camera,
+  CanvasMode,
+  CanvasState,
+  Color,
+  LayerType,
+  Point,
+  Side,
+  XYWH,
+} from "@/types";
+
 import { Info } from "@/components/canvas/Info";
 import { Participant } from "@/components/canvas/Participant";
 import { Toolbar } from "@/components/canvas/ToolBar";
@@ -9,11 +34,25 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participant />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
